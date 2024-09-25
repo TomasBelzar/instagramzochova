a50766395811c7843f6ec92b0d89eb8bd9bf671d
TomasBelzar <tbelzar541@gmail.com>
2024-09-25 13:55:49 +0200

    initial

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+// src/app/not-found.tsx
+
+ 
+import Typography  from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+
+
+export default function Home() {
+  
+  return (
+    <Container>
+    <Typography> not found</Typography>
+    </Container>
+  );
+}
---
+++ src/app/podmienky/page.tsx
@@ -0,0 +1,17 @@
+// src/app/podmienkz
+
+ 
+import Typography  from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+
+
+export default function Home() {
+  
+  return (
+    <Box>
+    <Typography> Podmienky používania</Typography>
+    </Box>
+
+  );
+}
